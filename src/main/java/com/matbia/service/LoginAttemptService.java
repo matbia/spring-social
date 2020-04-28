35c30d0b3cf5ee695442cc1b33bb62c8b36260ce
Mateusz Biały <mateusz.bialy@pm.me>
2020-04-28 22:38:10 +0200

    Update Javadoc

--- src/main/java/com/matbia/service/LoginAttemptService.java
+++ src/main/java/com/matbia/service/LoginAttemptService.java
@@ -8,11 +8,21 @@ import org.springframework.stereotype.Service;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Service class responsible for preventing brute-force attacks on user accounts.
+ * Does so by caching IP addresses of clients who provided incorrect login credentials.
+ */
 @Service
 public class LoginAttemptService {
+    /**
+     * Amount of allowed failed login attempts before client's IP address gets blocked
+     */
     private  final int MAX_ATTEMPT = 5;
     private LoadingCache<String, Integer> attemptsCache;
 
+    /**
+     * Constructor configures and builds the login attempts cache.
+     */
     public LoginAttemptService() {
         attemptsCache = CacheBuilder.newBuilder().
                 expireAfterWrite(3, TimeUnit.HOURS).build(new CacheLoader<String, Integer>() {
@@ -22,10 +32,18 @@ public class LoginAttemptService {
         });
     }
 
+    /**
+     * Resets login attempts counter of given IP by removing it from cache.
+     * @param key client's IP address
+     */
     public void loginSucceeded(String key) {
         attemptsCache.invalidate(key);
     }
 
+    /**
+     * Caches the IP, if it isn't already cached, and increments the attempts counter
+     * @param key client's IP address
+     */
     public void loginFailed(String key) {
         int attempts = 0;
         try {
@@ -37,6 +55,11 @@ public class LoginAttemptService {
         attemptsCache.put(key, attempts);
     }
 
+    /**
+     * Checks if given IP address is blocked
+     * @param key client's IP address
+     * @return true if login attempts exceed predetermined limit, false otherwise
+     */
     public boolean isBlocked(String key) {
         try {
             return attemptsCache.get(key) >= MAX_ATTEMPT;
